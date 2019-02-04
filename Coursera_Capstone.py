7b082d02569e80e4895c1f10b7f8f6d0237b77ee
Gustavo Bicalho <gustavocbicalho@gmail.comgit commit -m 'Adding the Coursera_Capstone file'git config --global user.email gustavocbicalho@gmail.com>
2019-02-04 16:22:51 -0200

    Adding the notebook and the python file for the Coursera Captone Project

---
+++ Coursera_Capstone.py
@@ -0,0 +1,16 @@
+#%% [markdown]
+# # This notebook will be used for the Capstone Project
+
+#%%
+import pandas as pd
+import numpy as np
+
+
+#%%
+print("Hello Capstone Project Course!")
+
+
+#%%
+
+
+
